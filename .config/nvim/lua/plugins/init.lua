25d1885cfa1f5c31d5bf1c16a31a7a940d1ecf22
James Gatter <jggatter@gmail.com>
2024-10-13 17:21:18 -0400

    nvim-surround plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -33,4 +33,14 @@ return {
   {
   	"williamboman/mason.nvim",
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
 }
