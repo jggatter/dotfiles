def8c71176d85af46ed0268c9f0ba5e1708e85ab
James Gatter <jggatter@gmail.com>
2025-10-20 11:50:07 -0400

    nvim: update lsp config for >=0.11

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,72 +1,61 @@
--- load defaults i.e lua_lsp
+-- Load NvChad LSP defaults
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
 
-local servers = { "html", "cssls", "ts_ls" }
-local nvlsp = require "nvchad.configs.lspconfig"
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
+-- List of servers to setup with default config
+local servers = { "html", "cssls", "ts_ls", "ruff_lsp" }
+
+for _, name in ipairs(servers) do
+  vim.lsp.config(name, {
+    on_attach    = nvlsp.on_attach,
+    on_init      = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
+  vim.lsp.enable(name)
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
-
--- python
-lspconfig.ruff.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
-
--- go
-lspconfig.gopls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = function(client, _)
-    -- Required for semanticTokens
-    -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+-- Go (gopls) with special semanticTokens fix
+vim.lsp.config("gopls", {
+  on_attach    = nvlsp.on_attach,
+  on_init      = function(client, _)
     if client.name == "gopls" then
       if not client.server_capabilities.semanticTokensProvider then
         local semantic = client.config.capabilities.textDocument.semanticTokens
         client.server_capabilities.semanticTokensProvider = {
-          full = true,
+          full   = true,
           legend = {
             tokenModifiers = semantic.tokenModifiers,
-            tokenTypes = semantic.tokenTypes
+            tokenTypes     = semantic.tokenTypes,
           },
           range = true,
         }
       end
     end
+    -- also call the standard on_init
+    if nvlsp.on_init then
+      return nvlsp.on_init(client, _)
+    end
   end,
   capabilities = nvlsp.capabilities,
   settings = {
-    -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
     gopls = {
-      semanticTokens = true,  -- experimental
+      semanticTokens = true,
       usePlaceholders = true,
       analyses = {
         unusedvariable = true,
       },
-      staticcheck = true,  -- experimental
-      hints = {  --experimental
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
+      staticcheck = true,
+      hints = {
+        assignVariableTypes      = true,
+        compositeLiteralFields   = true,
+        compositeLiteralTypes    = true,
+        constantValues           = true,
+        functionTypeParameters   = true,
+        parameterNames           = true,
+        rangeVariableTypes       = true,
       },
-    }
-  }
-}
+    },
+  },
+})
+vim.lsp.enable("gopls")
