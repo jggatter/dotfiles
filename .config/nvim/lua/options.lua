e77277ac996941f46b0c391aa6685f5f4d3cdb6a
James Gatter <jggatter@gmail.com>
2026-04-28 10:16:36 -0400

    feat(nvim): enable osc52 clipboard provider if mac vscode or ssh

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,19 +1,35 @@
 require "nvchad.options"
 
+-- Clipboard
+
+local uname = vim.loop.os_uname().sysname
+local is_ssh   = (vim.env.SSH_TTY ~= nil) or (vim.env.SSH_CONNECTION ~= nil)
+local in_tmux  = (vim.env.TMUX ~= nil)
+local in_vscode= (vim.env.TERM_PROGRAM == "vscode")
+
+local function enable_osc52()
+  local ok, osc52 = pcall(require, "vim.ui.clipboard.osc52")  -- nvim 0.10+
+  if ok then
+    vim.g.clipboard = {
+      name = "OSC 52",
+      copy  = {["+"] = osc52.copy("+"), ["*"] = osc52.copy("*")},
+      paste = {["+"] = osc52.paste("+"), ["*"] = osc52.paste("*")},
+    }
+  end
+end
 
 -- For VSCode Remote SSH clipboard support macOS <> Linux
--- Use OSC52 as the clipboard provider (copy + paste when supported)
-local ok, osc52 = pcall(require, "vim.ui.clipboard.osc52")
-if ok then
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {["+"] = osc52.copy("+"), ["*"] = osc52.copy("*")},
-    paste = {["+"] = osc52.paste("+"), ["*"] = osc52.paste("*")},
-  }
+-- Use OSC52 if we're remote (or inside tmux when remote).
+-- Locally on macOS, don't override clipboard; macOS provider (pbcopy/pbpaste) is fastest/stablest.
+if is_ssh or (in_vscode and uname ~= "Darwin") then
+  enable_osc52()
 end
+
+-- Let yanks use the + register by default everywhere
 vim.opt.clipboard = "unnamedplus"
 
---
+
+-- Cursor
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.guicursor = ''
 
@@ -39,7 +55,7 @@ vim.api.nvim_create_autocmd({"FileType"}, {
     end,
 })
 
--- 
+-- Undo, backup, and swap files
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
@@ -49,10 +65,10 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
---
+-- Appearance
 vim.opt.termguicolors = true
 
---
+-- Miscellaneous
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
